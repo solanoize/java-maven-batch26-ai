b26ea413aedc46808ac27e1d24be7ac78ccf969b
Solanoize <yanwarsolah@gmail.com>
2026-01-12 19:42:25 +0700

    done

--- src/main/java/com/juaracoding/testing/App.java
+++ src/main/java/com/juaracoding/testing/App.java
@@ -1,30 +1,41 @@
 package com.juaracoding.testing;
 
-import java.util.Arrays;
-
-import com.juaracoding.testing.model.Cart;
-import com.juaracoding.testing.model.Product;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class App {
     public static void main(String[] args) {
-        Product[] products = {
-                new Product(1, "Mie Ayam", 15000.00, 0.0, 10),
-                new Product(2, "Es Teh Tawar", 3000.00, 0.0, 100),
-                new Product(3, "Kerupuk Kulit", 5000.00, 0.0, 100),
-        };
+        ArrayList<String> daftarMobil = new ArrayList<>();
+        daftarMobil.add("Volvo"); // index 0
+        daftarMobil.add("BMW"); // index 1
+        daftarMobil.add("Toyota"); // index 2
 
-        Cart[] carts = {
-                new Cart(products[0], 2),
-                new Cart(products[1], 3)
-        };
+        // System.out.println("Isi daftar mobil = " + daftarMobil);
 
-        double total = 0;
-        System.out.println("======= STRUK MAKAN RESTO UHUY =======");
-        for (Cart cart : carts) {
-            System.out.printf("%s (%d)\t\t %.2f\n", cart.getProduct().getName(), cart.getQty(), cart.getSubtotal());
-            total = total + cart.getSubtotal();
+        // daftarMobil.set(2, "Daihatsu");
+
+        // System.out.println("Isi daftar mobil yang sudah diubah = " + daftarMobil);
+
+        // String bmw = daftarMobil.get(1);
+
+        // System.out.println("Isi nilai index 1 = " + bmw);
+
+        // int banyakItem = daftarMobil.size();
+        // System.out.println(banyakItem);
+
+        /**
+         * Looping array list cara 1
+         */
+        // for (int i = 0; i < daftarMobil.size(); i++) {
+        // System.out.println(daftarMobil.get(i));
+        // }
+
+        /**
+         * Looping array list cara 2
+         */
+        Collections.sort(daftarMobil);
+        for (String mobil : daftarMobil) {
+            System.out.println(mobil);
         }
-
-        System.out.printf("Total:\t\t Rp. %.2f\n", total);
     }
 }
