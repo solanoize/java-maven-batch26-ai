ef25cec883261f4d7b92a6ecafdd59fd0923ac50
Solanoize <yanwarsolah@gmail.com>
2026-01-12 20:08:39 +0700

    done

--- src/main/java/com/juaracoding/testing/App.java
+++ src/main/java/com/juaracoding/testing/App.java
@@ -1,40 +1,18 @@
 package com.juaracoding.testing;
 
-import java.util.LinkedList;
+import java.util.HashSet;
 
 public class App {
     public static void main(String[] args) {
-        LinkedList<String> daftarWarga = new LinkedList();
+        HashSet<String> data = new HashSet<>();
 
-        daftarWarga.add("Budi");
-        daftarWarga.add("Erni");
-        daftarWarga.add("Salim");
-        daftarWarga.add("Deni");
+        data.add("gajah");
+        data.add("ayam");
+        data.add("Kerbau");
+        data.add("kerbau");
+        data.add("kerbau");
+        data.add("kerbau");
 
-        System.out.println(daftarWarga);
-        System.out.println(daftarWarga.size());
-
-        daftarWarga.addFirst("Doni");
-
-        System.out.println(daftarWarga);
-        System.out.println(daftarWarga.size());
-
-        daftarWarga.addLast("Susi");
-
-        System.out.println(daftarWarga);
-        System.out.println(daftarWarga.size());
-
-        daftarWarga.set(3, "Jery");
-
-        System.out.println(daftarWarga);
-        System.out.println(daftarWarga.size());
-
-        daftarWarga.removeFirst();
-        System.out.println(daftarWarga);
-        System.out.println(daftarWarga.size());
-
-        daftarWarga.removeLast();
-        System.out.println(daftarWarga);
-        System.out.println(daftarWarga.size());
+        System.out.println(data);
     }
 }
